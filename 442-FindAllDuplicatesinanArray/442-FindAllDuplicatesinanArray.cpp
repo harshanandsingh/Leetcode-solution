2dcc789e5354313674b2aebcd035766d9060d5be
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2025-03-15 17:48:00 +0530

    File updated at 3/15/2025, 5:48:00 PM

--- 442-FindAllDuplicatesinanArray/442-FindAllDuplicatesinanArray.cpp
+++ 442-FindAllDuplicatesinanArray/442-FindAllDuplicatesinanArray.cpp
@@ -1,15 +1,32 @@
 class Solution {
 public:
+    // vector<int> findDuplicates(vector<int>& nums) {
+    //     int n = nums.size();
+    //     vector<int>v(n+1,0);
+    //     for(int i=0;i<n;i++){
+    //         v[nums[i]]++;
+    //     }
+    //     vector<int>ans;
+    //     for(int i=0;i<n+1;i++){
+    //         if(v[i]>=2) ans.push_back(i);
+    //     }
+    //     return ans;
+    // }
+
+
+
+public:
     vector<int> findDuplicates(vector<int>& nums) {
-        int n = nums.size();
-        vector<int>v(n+1,0);
-        for(int i=0;i<n;i++){
-            v[nums[i]]++;
+        vector<int> res;
+
+        for (int num : nums) {
+            int idx = abs(num) - 1;
+            if (nums[idx] < 0) {
+                res.push_back(abs(num));
+            }
+            nums[idx] = -nums[idx];
         }
-        vector<int>ans;
-        for(int i=0;i<n+1;i++){
-            if(v[i]>=2) ans.push_back(i);
-        }
-        return ans;
+        
+        return res;
     }
 };
