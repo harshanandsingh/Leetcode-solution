150d8153c5afbcccf6857c63e523d9723bc613b3
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2025-03-15 18:36:18 +0530

    File updated at 3/15/2025, 6:36:18 PM

--- 1822-SignoftheProductofanArray/1822-SignoftheProductofanArray.cpp
+++ 1822-SignoftheProductofanArray/1822-SignoftheProductofanArray.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
     int arraySign(vector<int>& nums) {
-        int n=nums.size();
-        long long int pro=1;
+        // int n=nums.size();
+        // long long int pro=1;
+        // for(int x:nums){
+        //     pro*=x;
+        //     if(pro) pro /= abs(pro);
+        //     else return 0;
+        // }
+        // if(pro<0) return -1;
+        // else if(pro >0) return 1;
+        // else return 0;
+
+        int sign =1;
         for(int x:nums){
-            pro*=x;
-            if(pro) pro /= abs(pro);
+            if(x==0) return 0;
+            else if(x<0) sign *=-1;
         }
-        if(pro<0) return -1;
-        else if(pro >0) return 1;
-        else return 0;
+        return sign;
     }
 };
