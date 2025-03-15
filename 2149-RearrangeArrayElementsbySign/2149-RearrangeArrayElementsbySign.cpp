cfed5e85f5efe14d7fe85158ce193fd0d79fafca
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2025-03-16 04:04:31 +0530

    File updated at 3/16/2025, 4:04:30 AM

--- 2149-RearrangeArrayElementsbySign/2149-RearrangeArrayElementsbySign.cpp
+++ 2149-RearrangeArrayElementsbySign/2149-RearrangeArrayElementsbySign.cpp
@@ -1,21 +1,38 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        vector<int>pve;
-        vector<int>nve;
+        // vector<int>pve;
+        // vector<int>nve;
 
-        int n = nums.size();
+        // int n = nums.size();
 
-        for(int i=0;i<n;i++){
-            if(nums[i]<0) nve.push_back(nums[i]);
-            else pve.push_back(nums[i]);
-        }
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]<0) nve.push_back(nums[i]);
+        //     else pve.push_back(nums[i]);
+        // }
 
-        int i=0,k=0;
+        // int i=0,k=0;
+        // while(k<n){
+        //     nums[k++] = pve[i];
+        //     nums[k++] = nve[i++];
+        // }
+        // return nums;
+
+        int i=0,j=1,k=0;
+        int n=nums.size();
+
+        vector<int>v(n);
         while(k<n){
-            nums[k++] = pve[i];
-            nums[k++] = nve[i++];
+            if(nums[k]>=0){
+                v[i]=nums[k++];
+                //cout<<v[i]<<" ";
+                i+=2;
+            }
+            else{
+                v[j]=nums[k++];
+                j+=2;
+            }
         }
-        return nums;
+        return v;
     }
 };
