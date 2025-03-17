68554cf177e656c23942509f99ce27ba299e79dc
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2025-03-17 08:36:36 +0530

    File updated at 3/17/2025, 8:36:36 AM

--- 128-LongestConsecutiveSequence/128-LongestConsecutiveSequence.cpp
+++ 128-LongestConsecutiveSequence/128-LongestConsecutiveSequence.cpp
@@ -1,19 +1,36 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        if (nums.empty()) return 0; // Handle empty case
+        // if (nums.empty()) return 0; // Handle empty case
     
-        sort(nums.begin(), nums.end());
+        // sort(nums.begin(), nums.end());
 
-        int count = 1, max_count = 1;
-        for (int i = 1; i < nums.size(); i++) {
-            if (nums[i] == nums[i - 1]) continue;
-            else if (nums[i] == nums[i - 1] + 1) count++;
-            else {
+        // int count = 1, max_count = 1;
+        // for (int i = 1; i < nums.size(); i++) {
+        //     if (nums[i] == nums[i - 1]) continue;
+        //     else if (nums[i] == nums[i - 1] + 1) count++;
+        //     else {
+        //         max_count = max(max_count, count);
+        //         count = 1;
+        //     }
+        // }
+        // return max(max_count, count); // Ensure last sequence is considered
+
+        unordered_set<int> numSet(nums.begin(), nums.end());
+        int max_count = 0;
+
+        for (int num : numSet) {
+            if (numSet.find(num - 1) == numSet.end()) { // Start of a sequence
+                int currentNum = num;
+                int count = 1;
+
+                while (numSet.find(currentNum + 1) != numSet.end()) {
+                    currentNum++;
+                    count++;
+                }
                 max_count = max(max_count, count);
-                count = 1;
             }
         }
-        return max(max_count, count); // Ensure last sequence is considered
+        return max_count;
     }
 };
