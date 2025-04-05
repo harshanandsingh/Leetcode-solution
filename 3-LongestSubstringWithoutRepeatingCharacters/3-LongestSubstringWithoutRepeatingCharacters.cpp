636c29aec0d5fde3ea1fdb2f4e7349584a7a52a1
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2025-04-05 11:37:58 +0530

    File updated at 4/5/2025, 11:37:59 AM

--- 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.cpp
+++ 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.cpp
@@ -1,3 +1,4 @@
+// Last updated: 4/5/2025, 11:37:59 AM
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
@@ -20,21 +21,40 @@ public:
         if(s1.length()>ans) ans=s1.length();
         return ans;*/
 
-        int n = s.size();
-        int i=0,j=0;
-        int max_len = 0;
+        // int n = s.size();
+        // int i=0,j=0;
+        // int max_len = 0;
+        // unordered_map<char,int>m;
+        // while(j<n){
+        //     if(m.find(s[j])!=m.end()){
+        //         max_len = max(max_len,j-i);
+        //         if(m[s[j]]+1 > i ) i = m[s[j]]+1;
+        //         //cout<<max_len<<" "<<endl;
+        //         //cout<<i<<" "<<j<<" "<<endl;
+        //     }
+        //     m[s[j]]=j;
+        //     j++;
+        // }
+        // max_len = max(max_len,j-i);
+        // return max_len;
+
+
         unordered_map<char,int>m;
+        int i=0,j=0,ans=0;
+        int n = s.size();
+
         while(j<n){
-            if(m.find(s[j])!=m.end()){
-                max_len = max(max_len,j-i);
-                if(m[s[j]]+1 > i ) i = m[s[j]]+1;
-                //cout<<max_len<<" "<<endl;
-                //cout<<i<<" "<<j<<" "<<endl;
+            m[s[j]]++;
+
+            while(m[s[j]]>1){
+                m[s[i]]--;
+                i++;
             }
-            m[s[j]]=j;
+
+            int window_size = j-i+1;
+            ans = max(ans,window_size);
             j++;
         }
-        max_len = max(max_len,j-i);
-        return max_len;
+        return ans;
     }
 };
