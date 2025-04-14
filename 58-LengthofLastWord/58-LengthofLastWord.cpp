dbd08a29e71dce1dc4c83defbbc7d5f1fa8ae2c3
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2025-04-14 19:20:54 +0530

    File updated at 4/14/2025, 7:20:55 PM

--- 58-LengthofLastWord/58-LengthofLastWord.cpp
+++ 58-LengthofLastWord/58-LengthofLastWord.cpp
@@ -1,15 +1,18 @@
+// Last updated: 4/14/2025, 7:20:55 PM
 class Solution {
 public:
     int lengthOfLastWord(string s) {
         int n = s.size();
-        int k=0,count=0;
-        for(int i=n-1;i>=0;i--){
-            if(s[i]!=' '){
-                k=1;
-                count++;
-            }
-            else if(s[i]==' ' && k==1) break;
+        
+        int i=n-1;
+        while(s[i]==' ') i--;
+        if(i<0) return 0;
+        int count=0;
+        for(;i>=0;i--){
+            if(s[i]!=' ') count++;
+            else break;
         }
+
         return count;
     }
 };
