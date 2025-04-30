cc1ab89d2dd34c70a6ec99feee2d1422462671e5
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2025-04-30 12:17:39 +0530

    File updated at 4/30/2025, 12:17:38 PM

--- 977-SquaresofaSortedArray/977-SquaresofaSortedArray.cpp
+++ 977-SquaresofaSortedArray/977-SquaresofaSortedArray.cpp
@@ -1,13 +1,34 @@
-// Last updated: 4/30/2025, 12:09:43 PM
+// Last updated: 4/30/2025, 12:17:38 PM
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
+        // int n = nums.size();
+
+        // for(int i=0;i<n;i++) nums[i]=nums[i]*nums[i];
+
+        // sort(nums.begin(),nums.end());
+
+        // return nums;
+
         int n = nums.size();
+        vector<int> result(n);
+        
+        int left = 0, right = n - 1;
+        int index = n - 1;
 
-        for(int i=0;i<n;i++) nums[i]=nums[i]*nums[i];
-
-        sort(nums.begin(),nums.end());
-
-        return nums;
+        while (left <= right) {
+            int leftSq = nums[left] * nums[left];
+            int rightSq = nums[right] * nums[right];
+            
+            if (leftSq > rightSq) {
+                result[index--] = leftSq;
+                left++;
+            } else {
+                result[index--] = rightSq;
+                right--;
+            }
+        }
+        
+        return result;
     }
 };
