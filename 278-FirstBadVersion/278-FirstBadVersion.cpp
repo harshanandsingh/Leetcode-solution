328603480bb83b24aa08f3fd5662f7f4ba2c5096
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2025-05-01 17:29:06 +0530

    File updated at 5/1/2025, 5:29:08 PM

--- 278-FirstBadVersion/278-FirstBadVersion.cpp
+++ 278-FirstBadVersion/278-FirstBadVersion.cpp
@@ -1,21 +1,19 @@
-// Last updated: 5/1/2025, 5:16:05 PM
+// Last updated: 5/1/2025, 5:29:08 PM
 // The API isBadVersion is defined for you.
 // bool isBadVersion(int version);
 
 class Solution {
 public:
     int firstBadVersion(int n) {
-        int low =1,high=n;
-        int ans = INT_MAX;
-        while(low<=high){
-            int mid = low + (high-low)/2;
-            bool res = isBadVersion(mid);
-            if(res){
-                ans = min(mid,ans);
-                high = mid-1;
-            }
-            else low=mid+1;
+        int start=1,end=n;
+        while(start<=end)
+        {
+            int mid=(end-start)/2+start;
+            if(isBadVersion(mid))
+                end=mid-1;
+            else 
+                start=mid+1;
         }
-        return ans;
+        return start;
     }
 };
