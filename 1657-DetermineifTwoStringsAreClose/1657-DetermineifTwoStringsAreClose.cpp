2089d0f1a57b64aec7ae618a9137fd12adaf35b3
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2025-07-13 09:27:55 +0530

    Create 1657-DetermineifTwoStringsAreClose

---
+++ 1657-DetermineifTwoStringsAreClose/1657-DetermineifTwoStringsAreClose.cpp
@@ -0,0 +1,26 @@
+// Last updated: 7/13/2025, 9:27:55 AM
+class Solution {
+public:
+    bool closeStrings(string word1, string word2) {
+        int n = word1.size();
+        if(word1.size() != word2.size()) return false;
+
+        vector<int>v1(26,0);
+        vector<int>v2(26,0);
+
+        for(auto x:word1) v1[x-'a']++;
+        for(auto x:word2) v2[x-'a']++;
+
+        for(int i=0;i<26;i++){
+            if((v1[i]!=0 && v2[i]==0) || (v1[i]==0 && v2[i]!=0)) return false;
+        }
+
+        sort(v1.begin(),v1.end(),greater<>());
+        sort(v2.begin(),v2.end(),greater<>());
+
+        for(int i=0;i<26;i++){
+            if(v1[i]!=v2[i]) return false;
+        }
+        return true;
+    }
+};
