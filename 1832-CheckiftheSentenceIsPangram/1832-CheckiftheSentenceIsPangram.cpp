acfa438fc69ba7cbc45f74b7cd74194b19e960f8
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2025-07-13 07:24:03 +0530

    File updated at 7/13/2025, 7:24:04 AM

--- 1832-CheckiftheSentenceIsPangram/1832-CheckiftheSentenceIsPangram.cpp
+++ 1832-CheckiftheSentenceIsPangram/1832-CheckiftheSentenceIsPangram.cpp
@@ -1,16 +1,24 @@
-// Last updated: 7/3/2025, 10:59:29 AM
+// Last updated: 7/13/2025, 7:24:04 AM
 class Solution {
 public:
     bool checkIfPangram(string s) {
+        // int n = s.size();
+        // if(n<26) return false;
+        // vector<int>v(26,0);
+        // for(auto x:s){
+        //     v[x-'a']++;
+        // }
+        // for(auto x:v){
+        //     if(x==0) return false;
+        // }
+        // return true;
+
+        vector<int>v(26);
         int n = s.size();
-        if(n<26) return false;
-        vector<int>v(26,0);
         for(auto x:s){
             v[x-'a']++;
         }
-        for(auto x:v){
-            if(x==0) return false;
-        }
+        for(auto x:v) if(x==0) return false;
         return true;
 
     }
