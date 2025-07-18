4f097f7b9acfe0290687d84c2d72816b93cb20c1
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2025-07-18 07:34:08 +0530

    Create 210-CourseScheduleII

---
+++ 210-CourseScheduleII/210-CourseScheduleII.cpp
@@ -0,0 +1,43 @@
+// Last updated: 7/18/2025, 7:34:09 AM
+class Solution {
+public:
+    vector<int> findOrder(int n, vector<vector<int>>& p) {
+        // make adjency list -> 
+        vector<vector<int>>adj(n);
+        for(auto x:p){
+            adj[x[1]].push_back(x[0]);
+        }
+
+        // find indegree of all the subject 
+        vector<int>indegree(n,0);
+        for(auto x:adj){
+            for(auto y:x){
+                indegree[y]++;
+            }
+        }
+
+        // search for the node who have 0 indegree and push it in the queue 
+        queue<int>q;
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+        // create one vector to store the answer 
+        vector<int>ans;
+        while(!q.empty()){
+            int a = q.front();
+            ans.push_back(a);
+            q.pop();
+            for(auto x:adj[a]){
+                indegree[x]--;
+                if(indegree[x]==0){
+                    q.push(x);
+                }
+            }
+        }
+
+        if(ans.size()==n) return ans;
+        return {};
+    }
+};
