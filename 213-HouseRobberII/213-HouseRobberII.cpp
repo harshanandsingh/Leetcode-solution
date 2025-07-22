aaddf3361f7df688203a8b5c090a52dab220c689
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2025-07-22 12:39:35 +0530

    Create 213-HouseRobberII

---
+++ 213-HouseRobberII/213-HouseRobberII.cpp
@@ -0,0 +1,24 @@
+// Last updated: 7/22/2025, 12:39:36 PM
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n==1) return nums[0];
+        if(n==2) return max(nums[0],nums[1]);
+
+        vector<int>dp(n);
+        dp[0]=nums[0];
+        dp[1]=max(nums[0],nums[1]);
+        for(int i=2;i<n-1;i++){
+            dp[i]=max(nums[i]+dp[i-2],dp[i-1]);
+        }
+        int a = dp[n-2];
+        dp[1]=nums[1];
+        dp[2]=max(nums[1],nums[2]);
+        for(int i=3;i<n;i++){
+            dp[i]=max(nums[i]+dp[i-2],dp[i-1]);
+        }
+        int b = dp[n-1];
+        return max(a,b);
+    }
+};
