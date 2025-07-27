0dd32045e87af72e3cc9358380fbfd24fe7c18c4
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2025-07-27 09:19:19 +0530

    File updated at 7/27/2025, 9:19:21 AM

--- 424-LongestRepeatingCharacterReplacement/424-LongestRepeatingCharacterReplacement.cpp
+++ 424-LongestRepeatingCharacterReplacement/424-LongestRepeatingCharacterReplacement.cpp
@@ -1,4 +1,4 @@
-// Last updated: 4/5/2025, 12:14:44 PM
+// Last updated: 7/27/2025, 9:19:21 AM
 class Solution {
 public:
     int characterReplacement(string s, int k) {
@@ -6,15 +6,19 @@ public:
         unordered_map<char, int> count;
 
         while (j < s.size()) {
+            // increse the frequency of the current element 
             count[s[j]]++;
+            // now find the max frequeny in the current window 
             maxFreq = max(maxFreq, count[s[j]]);
 
-            // If changes needed > k, shrink from left
-            if ((j - i + 1) - maxFreq > k) {
+            // if the size of the window - max frequency is more then k -> then we have used the option of convering more then k 
+            while((j - i + 1) - maxFreq > k) {
+                // so we have to decrese the size of the window -> now we reduce the size of the window 
                 count[s[i]]--;
                 i++;
             }
 
+            // now check is it a max lenght substrng -> if so update the maxlength else let it be as it is 
             maxLen = max(maxLen, j - i + 1);
             j++;
         }
