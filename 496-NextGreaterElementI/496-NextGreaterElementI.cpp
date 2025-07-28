fa3066037f1274a1a4761b01ebcff934ee0e48ca
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2025-07-28 08:12:38 +0530

    File updated at 7/28/2025, 8:12:37 AM

--- 496-NextGreaterElementI/496-NextGreaterElementI.cpp
+++ 496-NextGreaterElementI/496-NextGreaterElementI.cpp
@@ -1,24 +1,45 @@
-// Last updated: 7/28/2025, 7:51:51 AM
+// Last updated: 7/28/2025, 8:12:37 AM
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector<int>ans;
-        for(auto x:nums1){
-            bool flag = true;
-            auto it = find(nums2.begin(),nums2.end(),x);
-            while(it<nums2.end()){
-                if(*it > x){
-                    ans.push_back(*it);
-                    flag = false;
-                    break;
-                }
-                it++;
-            }
-            // if we don't find the element greater then x 
-            if(flag){
-                ans.push_back(-1);
-            }
+
+        unordered_map<int,int>m; // to map with next greater element 
+        stack<int>s; // to find next greater element 
+
+        for(int i = nums2.size()-1 ;i>=0;i--){// start form the last element 
+            // if stack is not empty and top of the stack is less then nums2[i] pop it out -> as we only have to store the 
+            // element which is greater then nums2[i] 
+            while(!s.empty() && nums2[i]>= s.top()) s.pop();
+
+            m[nums2[i]] = s.empty() ? -1 : s.top();
+            s.push(nums2[i]);
         }
-        return ans;
+        vector<int>v;
+        for(auto x:nums1){
+            v.push_back(m[x]);
+        }
+        return v;
+        // vector<int>ans;
+        // // intrate through the nums1 element 
+        // for(auto x:nums1){
+        //     bool flag = true;
+        //     // find the elment in the nums2 array 
+        //     auto it = find(nums2.begin(),nums2.end(),x);
+        //     // find if any elemnt present with more value then x -> at index more then what we find out
+        //     while(it<nums2.end()){
+        //         if(*it > x){
+        //             ans.push_back(*it);
+        //             // if we fond out no need to add -1 
+        //             flag = false;
+        //             break;
+        //         }
+        //         it++;
+        //     }
+        //     // if we don't find the element greater then x add -1 
+        //     if(flag){
+        //         ans.push_back(-1);
+        //     }
+        // }
+        // return ans;
     }
 };
