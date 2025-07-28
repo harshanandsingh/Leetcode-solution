dc2ab6453f92e5e306efebdd0cc299aec821a404
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2025-07-28 09:39:51 +0530

    File updated at 7/28/2025, 9:39:50 AM

--- 2559-CountVowelStringsinRanges/2559-CountVowelStringsinRanges.cpp
+++ 2559-CountVowelStringsinRanges/2559-CountVowelStringsinRanges.cpp
@@ -1,31 +1,35 @@
-// Last updated: 7/28/2025, 9:35:05 AM
+// Last updated: 7/28/2025, 9:39:50 AM
 class Solution {
 public:
-    int start_end_vow(string s){
-        char b = *(s.begin()), e = *(s.end()-1);
-        if( (b == 'a' || b == 'e' || b == 'i' || b=='o' || b=='u')  && (e == 'a' || e == 'e' || e == 'i' || e== 'o' ||e== 'u') ){
-            return 1;
-        }
-        return 0;
-    }
     vector<int> vowelStrings(vector<string>& words, vector<vector<int>>& queries) {
-        // find a function to find the string which is starting and ending wirh a vovel 
-        int n = words.size();
-        // vecotr to store prefix sum of all the vovel till that point 
-        vector<int>v(n,0);
-        int pre_sum=0;
-        for(int i=0;i<n;i++){
-            pre_sum += start_end_vow(words[i]);
-            v[i] = pre_sum;
+        
+        int size = words.size();
+        vector<int> pre(size, 0);
+
+
+        for(int i = 0; i < words.size(); i++){
+            int sz = words[i].size();
+            if((words[i][0] == 'a' || words[i][0] == 'e' || words[i][0] == 'i' || words[i][0] == 'o' || words[i][0] == 'u') && (words[i][sz-1] == 'a' || words[i][sz-1] == 'e' || words[i][sz-1] == 'i' || words[i][sz-1] == 'o' || words[i][sz-1] == 'u')){
+                pre[i] = 1;
+            }
+            if(i > 0)
+            pre[i] += pre[i-1];
         }
 
-        vector<int>ans;
-        for(auto x:queries){
-            int a ;
-            if(x[0] != 0) a = v[x[1]] - v[x[0]-1];
-            else a = v[x[1]];
-            ans.push_back(a);
+        vector<int> res;
+
+        for(int i = 0; i < queries.size(); i++){
+            int l = queries[i][0];
+            int r = queries[i][1];
+            
+            int ans = 0;
+            ans = pre[r];
+            if(l > 0) ans -= pre[l-1];
+            
+            res.push_back(ans);
         }
-        return ans;
+
+        return res;
+
     }
 };
