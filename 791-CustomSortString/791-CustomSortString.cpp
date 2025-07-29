1a43a1f395860e2b030cc407a3675c06910203aa
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2025-07-29 15:08:15 +0530

    File updated at 7/29/2025, 3:08:15 PM

--- 791-CustomSortString/791-CustomSortString.cpp
+++ 791-CustomSortString/791-CustomSortString.cpp
@@ -1,47 +1,17 @@
-// Last updated: 7/29/2025, 3:01:10 PM
+// Last updated: 7/29/2025, 3:08:15 PM
 class Solution {
 public:
-    bool cmp(const pair<int, pair<int, char>> &a, const pair<int, pair<int, char>> &b) {
-        return a.first < b.first;
+
+    static string str;
+
+    static bool compare(char ch1, char ch2){
+        return str.find(ch1) < str.find(ch2);
     }
+
     string customSortString(string order, string s) {
-        // 1. we need position 
-        // 2. we need frequency 
-        // 3. char 
-
-        // size of an order array -> which contain only unique element 
-        int n = order.size();
-        // to store the index of charector in order string 
-        vector<int>v(26,0);
-        for(int i = 0;i<n;i++){
-            v[order[i] - 'a'] = i;
-        }
-        // now we have the postion on the basis of which we have to sort 
-
-        // find the frequency of the char in main string s 
-        unordered_map<char,int>m;
-        for(auto x:s){
-            m[x]++;
-        }
-
-        // now we have to push each lement in the vector with its position , frequency , and char 
-        vector<pair<int, pair<int , char>>>e;
-        for(auto x:m){
-            char ch = x.first;
-            int a = x.second;
-            e.push_back( { v[ch-'a'] , {a , ch} });
-        }
-
-        // sort element based on pasition 
-        sort(e.begin(),e.end());
-
-        string ans = "";
-        for(auto x:e){
-            auto [a,b] = x;
-
-            ans+= string(b.first,b.second);
-        }
-        return ans;
-
+        str = order;
+        sort(s.begin(), s.end(), compare);
+        return s;
     }
 };
+string Solution::str;
