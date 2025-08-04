481cd67b93c6db71188d5bbdcdd79f4f574432d8
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2025-08-04 12:52:43 +0530

    File updated at 8/4/2025, 12:52:43 PM

--- 948-BagofTokens/948-BagofTokens.cpp
+++ 948-BagofTokens/948-BagofTokens.cpp
@@ -1,4 +1,4 @@
-// Last updated: 6/6/2025, 12:58:46 PM
+// Last updated: 8/4/2025, 12:52:43 PM
 class Solution {
 public:
     int bagOfTokensScore(vector<int>& t, int p) {
@@ -8,16 +8,17 @@ public:
         int s=0;
         int i=0,j=n-1;
         while(i<=j){
-            if(p>=t[i]){
+            if(p>=t[i]){ // till i have  power i will lose my power to gian as much score i can 
                 p-=t[i++];
                 s++;
-                max_s = max(max_s,s);
+                max_s = max(max_s,s); // find the max score each time i gain a score 
             }
-            else if(s>=1){
-                p+=t[j--];
+            else if(s>=1){ // if my score is >=1 i can loose score to gian more power 
+                p+=t[j--]; // loose score to gain most power 
                 s--;
             }
             else{
+                // when i dont have power lefet and also i dont have score >= 1 so i cnat do anything so break 
                 break;
             }
         }
