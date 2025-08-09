3a8d5c276450fabfed893fd8a1f52f3707d5a627
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2025-08-09 10:14:34 +0530

    File updated at 8/9/2025, 10:14:35 AM

--- 33-SearchinRotatedSortedArray/33-SearchinRotatedSortedArray.cpp
+++ 33-SearchinRotatedSortedArray/33-SearchinRotatedSortedArray.cpp
@@ -1,7 +1,7 @@
-// Last updated: 8/9/2025, 10:09:51 AM
+// Last updated: 8/9/2025, 10:14:35 AM
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
+  int search(vector<int>& nums, int target) {
         int n=nums.size();
         int low=0,high=n-1;
         while(low<=high){
@@ -11,13 +11,13 @@ public:
 
             // Check which part of the array is sorted
             if (nums[low] <= nums[mid]) { // Left half is sorted
-                if (target >= nums[low] && target < nums[mid]) {
+                if (target >= nums[low]  && target < nums[mid] ) {
                     high = mid - 1; // Target is in the left half
                 } else {
                     low = mid + 1; // Target is in the right half
                 }
             } else { // Right half is sorted
-                if (target > nums[mid] && target <= nums[high]) {
+                if ( target <= nums[high] &&  target > nums[mid]) {
                     low = mid + 1; // Target is in the right half
                 } else {
                     high = mid - 1; // Target is in the left half
@@ -25,5 +25,6 @@ public:
             }
         }
         return -1;
-    }
+    }  
 };
+
