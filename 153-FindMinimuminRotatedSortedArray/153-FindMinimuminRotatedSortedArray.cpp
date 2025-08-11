ad2ea1ad3b2cab18672d6f1419f64bb88c812236
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2025-08-11 08:29:06 +0530

    File updated at 8/11/2025, 8:29:06 AM

--- 153-FindMinimuminRotatedSortedArray/153-FindMinimuminRotatedSortedArray.cpp
+++ 153-FindMinimuminRotatedSortedArray/153-FindMinimuminRotatedSortedArray.cpp
@@ -1,7 +1,29 @@
-// Last updated: 8/11/2025, 8:05:52 AM
+// Last updated: 8/11/2025, 8:29:06 AM
 class Solution {
 public:
     int findMin(vector<int>& nums) {
+        // we can solve this problem , with the concept of peack element as well 
+        // no we cant solve uisng the peak element -> 
+        // int n = nums.size();
+        // int low = 0, high = n-1;
+        // int p;
+        // while(low<=high){
+        //     int mid = low + (high - low)/2;
+
+        //     if( (mid==0 || nums[mid-1]<nums[mid]) && (mid==n-1  || nums[mid] > nums[mid+1]) ){
+        //         p=mid;
+        //         break;
+        //     }
+        //     else if(mid < n-1 && nums[mid]<nums[mid+1]) low = mid+1;
+        //     else if(mid > 0 && nums[mid] < nums[mid-1]) high = mid-1;
+        // }
+        // cout<<p<<endl;
+        // return p == n-1 ? nums[0] : nums[p+1];
+
+
+
+
+        // this is one of the way to find the point of the sepration 
         int low = 0, high = nums.size() - 1;
 
         while (low < high) {
@@ -18,6 +40,6 @@ public:
         }
         
         // When low == high, we have found the minimum element
-        return nums[low];
+        return nums[high];
     }
 };
