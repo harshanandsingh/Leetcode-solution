330521ae266228e4c9c6ad02e130d62800e4e703
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2025-08-12 17:44:46 +0530

    File updated at 8/12/2025, 5:44:46 PM

--- 2439-MinimizeMaximumofArray/2439-MinimizeMaximumofArray.cpp
+++ 2439-MinimizeMaximumofArray/2439-MinimizeMaximumofArray.cpp
@@ -1,44 +1,16 @@
-// Last updated: 8/12/2025, 5:34:09 PM
+// Last updated: 8/12/2025, 5:44:46 PM
 class Solution {
 public:
-    bool pos(vector<int> nums,int k){
-        int n = nums.size();
-        // for(int i = 1;i<n;i++){
-        //     if(nums[i]>k){
-        //         int diff = nums[i]-k;
-        //         if(nums[i-1]+diff > k) return false;
-        //     }
-        // }
-        // return true;
-        long long int sum = 0;
-        for(int i = n-1;i>0;i--){
-            sum+=nums[i];
-            if(sum>k){
-                long long int diff = sum-k;
-                //cout<<sum<<" "<<diff<<endl;
-                sum = diff;
-            }
-            else sum = 0;
-        }
-        return sum+nums[0] <= k;
-    }
     int minimizeArrayValue(vector<int>& nums) {
-        // you can solve this also -> binery search on answer 
-        // find the range -> of the posible answer 
-        int low = nums[0];
-        int high = *max_element(nums.begin(),nums.end());
-        int ans = high;
+        int ans = 0;
+        long long sum = 0;
 
-        while(low<=high){
-            int mid = low + (high - low)/2;
-            if(pos(nums,mid)){
-                ans = mid ;
-                high = mid - 1;
-            }
-            else{
-                low = mid + 1;
-            }
+        for (int i = 0; i < nums.size(); ++i) {
+            sum += nums[i];
+            int avg = (sum + i) / (i + 1);
+            ans = max(ans, avg);
         }
-        return ans;
+
+        return max(ans, nums[0]);
     }
 };
