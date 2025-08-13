f790cb0830c169503a761950b531955e0f47f683
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2025-08-13 10:45:32 +0530

    File updated at 8/13/2025, 10:45:33 AM

--- 198-HouseRobber/198-HouseRobber.cpp
+++ 198-HouseRobber/198-HouseRobber.cpp
@@ -1,35 +1,17 @@
-// Last updated: 7/22/2025, 11:50:48 AM
+// Last updated: 8/13/2025, 10:45:33 AM
 class Solution {
 public:
-    // int rob(vector<int>& nums) {
-    //     wrong attempt , but good you atlist understand that why even and odd will not work 
-    //     int n = nums.size();
-    //     int e_total = 0;
-    //     int o_total = 0;
-    //     int e=0;
-    //     while(e<n){
-    //         e_total+=nums[e];
-    //         e+=2;
-    //     }
-    //     int o=1;
-    //     while(o<n){
-    //         o_total+=nums[o];
-    //         o+=2;
-    //     }
-    //     return max(e_total,o_total);
-    // }
-
-    int rob(vector<int>& nums){
+    int rob(vector<int>& nums) {
         int n = nums.size();
-        if(n==1)  return nums[0];
-        if(n==2) return max(nums[0],nums[1]);
-
         vector<int>dp(n);
+
+        if(n==1) return nums[0];
+        else if(n==2) return max(nums[0],nums[1]);
+        
         dp[0]=nums[0];
         dp[1]=max(nums[0],nums[1]);
-
         for(int i=2;i<n;i++){
-            dp[i]=max(dp[i-1],nums[i]+dp[i-2]);
+            dp[i]=max(dp[i-1] ,  dp[i-2]+nums[i]);
         }
         return dp[n-1];
     }
