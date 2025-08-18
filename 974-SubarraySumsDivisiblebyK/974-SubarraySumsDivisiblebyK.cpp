cda279543e5d1c3059141849b25552eedd50fd3a
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2025-08-18 11:21:46 +0530

    File updated at 8/18/2025, 11:21:47 AM

--- 974-SubarraySumsDivisiblebyK/974-SubarraySumsDivisiblebyK.cpp
+++ 974-SubarraySumsDivisiblebyK/974-SubarraySumsDivisiblebyK.cpp
@@ -1,4 +1,4 @@
-// Last updated: 8/18/2025, 11:18:04 AM
+// Last updated: 8/18/2025, 11:21:47 AM
 class Solution {
 public:
     int subarraysDivByK(vector<int>& nums, int k) {
@@ -13,14 +13,17 @@ public:
             long long int ele = sum%k;
             // if remander is < 0 , it will give wrong answer 
             if(ele < 0) ele+=k; // this will give a wright value 
-            if(m.find(ele)!=m.end()){
-                // we alrady have the same remander -> we will add that number of count and increase the remander count 
-                ans+=m[ele];
-                m[ele]++; // increase the count of the element 
-            }
-            else{
-                m[ele]=1; // we have to inser the reminder in the array 
-            }
+            // we don't need this 
+            // if(m.find(ele)!=m.end()){
+            //     // we alrady have the same remander -> we will add that number of count and increase the remander count 
+            //     ans+=m[ele];
+            //     m[ele]++; // increase the count of the element 
+            // }
+            // else{
+            //     m[ele]=1; // we have to inser the reminder in the array 
+            // }
+            ans+=m[ele];// this will give count value or 0 (give 0 if its not exist)
+            m[ele]++; // this will increase the reminder count 
         }
         return ans;
     }
