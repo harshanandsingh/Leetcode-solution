e8656aca514ec20c4d6e3a8a88f84b485241d993
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2025-08-18 15:31:29 +0530

    File updated at 8/18/2025, 3:31:31 PM

--- 1913-MaximumProductDifferenceBetweenTwoPairs/1913-MaximumProductDifferenceBetweenTwoPairs.cpp
+++ 1913-MaximumProductDifferenceBetweenTwoPairs/1913-MaximumProductDifferenceBetweenTwoPairs.cpp
@@ -1,9 +1,28 @@
-// Last updated: 8/18/2025, 3:18:57 PM
+// Last updated: 8/18/2025, 3:31:31 PM
 class Solution {
 public:
     int maxProductDifference(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int n = nums.size();
-        return nums[n-1]*nums[n-2] - nums[0]*nums[1];
+        int largest= 0;
+        int slargest =0;
+        int smallest =INT_MAX;
+        int ssmallest =INT_MAX;
+        for(int i=0; i<nums.size();i++){
+            if(largest < nums[i]){
+                slargest = largest;
+                largest = nums[i];
+            }
+            else if(slargest < nums[i]){
+                slargest = nums[i];
+            }
+            if(nums[i] < smallest){
+                ssmallest = smallest;
+                smallest = nums[i];
+            }
+           else if(ssmallest > nums[i]){
+                ssmallest = nums[i];
+            }
+            
+        }
+        return (largest*slargest) - (smallest *ssmallest); 
     }
 };
