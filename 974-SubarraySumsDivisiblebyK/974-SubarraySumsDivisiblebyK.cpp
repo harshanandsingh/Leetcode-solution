3a2369efc22a9fc85f0cdd1cf75d0195e40c7f5e
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2025-08-18 11:29:41 +0530

    File updated at 8/18/2025, 11:29:42 AM

--- 974-SubarraySumsDivisiblebyK/974-SubarraySumsDivisiblebyK.cpp
+++ 974-SubarraySumsDivisiblebyK/974-SubarraySumsDivisiblebyK.cpp
@@ -1,30 +1,16 @@
-// Last updated: 8/18/2025, 11:21:47 AM
+// Last updated: 8/18/2025, 11:29:42 AM
 class Solution {
 public:
     int subarraysDivByK(vector<int>& nums, int k) {
-        // this is also same as subarray sum equals to target 
-
-        unordered_map<int,int>m; // we need unordered map -> to store the remander and there count 
-        m[0]=1; // store count of 0 = 1 
-        long long int sum=0;
-        int ans=0;
-        for(auto x:nums){
-            sum+=x;
-            long long int ele = sum%k;
-            // if remander is < 0 , it will give wrong answer 
-            if(ele < 0) ele+=k; // this will give a wright value 
-            // we don't need this 
-            // if(m.find(ele)!=m.end()){
-            //     // we alrady have the same remander -> we will add that number of count and increase the remander count 
-            //     ans+=m[ele];
-            //     m[ele]++; // increase the count of the element 
-            // }
-            // else{
-            //     m[ele]=1; // we have to inser the reminder in the array 
-            // }
-            ans+=m[ele];// this will give count value or 0 (give 0 if its not exist)
-            m[ele]++; // this will increase the reminder count 
+        int prefixMod = 0;
+        vector<int> r(k+1);
+        int res = 0;
+        r[0] = 1;
+        for(int n: nums) {
+            prefixMod = (prefixMod + n %k + k) % k;
+            res += r[prefixMod];
+            r[prefixMod]++;
         }
-        return ans;
+        return res;
     }
 };
