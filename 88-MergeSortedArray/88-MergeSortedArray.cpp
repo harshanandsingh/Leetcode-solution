8ebefef054e09c7d6e2c2745ac49ae3ab879ed95
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2025-08-21 07:16:58 +0530

    File updated at 8/21/2025, 7:16:58 AM

--- 88-MergeSortedArray/88-MergeSortedArray.cpp
+++ 88-MergeSortedArray/88-MergeSortedArray.cpp
@@ -1,14 +1,27 @@
-// Last updated: 5/4/2025, 7:05:16 PM
+// Last updated: 8/21/2025, 7:16:58 AM
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i=m-1,j=0;
-        while(i>=0 && j<n){
-            if(nums1[i]>nums2[j]) swap(nums1[i--],nums2[j++]);
-            else break;
+        // 1. put all element in the same array and do sorting -> o(nlgon(n)) time 
+        // 2. use extra space and do murge sort -> o(n) but with o(n) space
+        // 3. better to start form the back -> 
+        int i=m-1; // first array largest element 
+        int j=n-1; // second element largest element 
+        int k = n+m-1; // start filling from the end 
+
+        while(i>=0 && j>=0){
+            if(nums1[i]>=nums2[j]){
+                nums1[k]=nums1[i];
+                k--;
+                i--;
+            }else{
+                nums1[k]=nums2[j];
+                k--;
+                j--;
+            }
         }
-        sort(nums1.begin(),nums1.begin()+m);
-        sort(nums2.begin(),nums2.end());
-        for(int i=m;i<n+m;i++) nums1[i]=nums2[i-m];
+        while(j>=0){
+            nums1[k--]=nums2[j--];
+        }
     }
 };
