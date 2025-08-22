a6a9d6112629c2aba530127ec35c05c262ec80d5
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2025-08-22 11:36:34 +0530

    File updated at 8/22/2025, 11:36:34 AM

--- 958-CheckCompletenessofaBinaryTree/958-CheckCompletenessofaBinaryTree.cpp
+++ 958-CheckCompletenessofaBinaryTree/958-CheckCompletenessofaBinaryTree.cpp
@@ -1,4 +1,4 @@
-// Last updated: 8/2/2025, 4:07:39 PM
+// Last updated: 8/22/2025, 11:36:34 AM
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -12,7 +12,34 @@
  */
 class Solution {
 public:
+    //int total_count=1;
+    int total_node(TreeNode* root){
+        if(!root) return 0;
+        return 1+total_node(root->left)+total_node(root->right);
+    }
+    int total;
+    bool dfs(TreeNode* root , int index){
+        if(!root) return true;
+        
+        if(index > total) return false;
+        //total_count++;
+
+        return dfs(root->left,2*index) && dfs(root->right,2*index+1);
+    }
     bool isCompleteTree(TreeNode* root) {
+        // lets try dfs 
+        // left child index = 2*i and right child index = 2*i+1
+        // find the total_node 
+        total = total_node(root);
+        return dfs(root,1);
+
+    }
+
+
+
+
+
+    bool isCompleteTree1(TreeNode* root) {
         // lets do level order traversal 
         // if we get any node thich is null -> and after that we find any node which is not null return flase 
         // logic -> null should be the last element to be sheen no element having null should be sheen after any nullptr 
@@ -27,9 +54,12 @@ public:
                 q.pop();
                 if(a==nullptr) null_p = true; // whenever we get the null we will make null present to true 
                 if(a != nullptr && null_p ) return false; // when there is more element after the null is present 
-
-
-                if(a != nullptr ) {
+                
+                // if(!a){
+                    // this will not work -> as the q will not be empty enven in complet tree 
+                //     return q.empty();
+                // }
+                if(a){
                     q.push(a->left);
                     q.push(a->right);
                 }
@@ -40,3 +70,11 @@ public:
     }
     
 };
+
+
+
+
+//                 if(a != nullptr ) {
+//                     q.push(a->left);
+//                     q.push(a->right);
+//                 }
