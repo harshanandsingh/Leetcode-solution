24815f8d32a65ee00534e893cff37531515e49b1
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2025-08-26 22:42:37 +0530

    File updated at 8/26/2025, 10:42:36 PM

--- 2244-MinimumRoundstoCompleteAllTasks/2244-MinimumRoundstoCompleteAllTasks.cpp
+++ 2244-MinimumRoundstoCompleteAllTasks/2244-MinimumRoundstoCompleteAllTasks.cpp
@@ -1,16 +1,16 @@
-// Last updated: 8/26/2025, 10:39:34 PM
+// Last updated: 8/26/2025, 10:42:36 PM
 class Solution {
 public:
-    int minimumRounds(vector<int>& tasks) {
-        unordered_map<int,int>m;
-        for(auto x:tasks){
-            m[x]++;
+    int minimumRounds(vector<int>& tasks){
+        unordered_map<int,int>mp;
+        for(auto &num:tasks){
+            mp[num]++;
         }
-        int ans = 0;
-        for(auto [a,b] : m){
-            if(b==1) return -1;
-            ans+= b/3;
-            if(b%3) ans+=1;
+        int ans=0;
+        for(auto it:mp){
+            if(it.second==1) return -1;
+            ans+=ceil(it.second/3.0); // yeha 3.0 (point zero dena jaruri hai)
+            // nhi toh integer ajaega float nhi ayega aur ceiling kaam nhi karega
         }
         return ans;
     }
