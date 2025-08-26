ea48030130719237260c35b4500ddcdd006928ae
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2025-08-26 17:30:28 +0530

    File updated at 8/26/2025, 5:30:28 PM

--- 1578-MinimumTimetoMakeRopeColorful/1578-MinimumTimetoMakeRopeColorful.cpp
+++ 1578-MinimumTimetoMakeRopeColorful/1578-MinimumTimetoMakeRopeColorful.cpp
@@ -1,13 +1,31 @@
-// Last updated: 8/4/2025, 6:06:55 PM
+// Last updated: 8/26/2025, 5:30:28 PM
 class Solution {
 public:
     int minCost(string c, vector<int>& t) {
+        int n = c.size();
+        int i=0,j=0;
+        int ans = 0;
+        while(j<n){
+            char ch = c[i];
+            int max_v = INT_MIN;
+            long long int sum = 0;
+            while(j<n && c[j]==ch){
+                max_v = max(t[j],max_v);
+                sum+= t[j];
+                j++;
+            }
+            if(j-i > 1) ans += sum - max_v;
+            i=j;
+        }
+        return ans;
+    }
+    int minCost1(string c, vector<int>& t) {
         // what i got an idea that -> find two consecutive -> remove the lowest time taking one 
         // now there might be a case like ->  a a a three consequtive -> time 2 1 3 -> so you removed 1 
         // now 2 3 are still there with same color -> agin have to remove -> but you cant check lieck like arr[i] == arr[i-1]
         // as arr[i-1] is no more , if you physicaly delete in given array -> it will take lots amount of time 
 
-        // so just sote the last visited index index 
+        // so just sote the last visited index  
         int n = c.size();
         int l = 0; // last index visited 
         int j=1;
