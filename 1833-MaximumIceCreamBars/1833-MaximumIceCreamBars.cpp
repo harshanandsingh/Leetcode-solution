f758ed07e6fc37e1bef96baab973823fd8dd7dca
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2025-08-26 23:07:57 +0530

    File updated at 8/26/2025, 11:07:57 PM

--- 1833-MaximumIceCreamBars/1833-MaximumIceCreamBars.cpp
+++ 1833-MaximumIceCreamBars/1833-MaximumIceCreamBars.cpp
@@ -1,16 +1,22 @@
-// Last updated: 8/26/2025, 10:55:49 PM
+// Last updated: 8/26/2025, 11:07:57 PM
 class Solution {
 public:
     int maxIceCream(vector<int>& costs, int coins) {
-        sort(costs.begin(),costs.end());
-        int ans = 0;
-        for(auto x:costs){
-            if(coins >=x){
-                coins-=x;
-                ans++;
+        int maxCost = 0;
+        for (int& i : costs) maxCost = max(maxCost, i);
+        vector<int> tally(maxCost + 1, 0);
+        for (int& i : costs) tally[i]++;
+        int result = 0;
+        int i = 1;
+        while (i <= maxCost && i <= coins){
+            if (tally[i]){
+                tally[i]--;
+                result++;
+                coins -= i;
+                //cout << i << endl;
             }
-            else return ans;
+            else i++;
         }
-        return ans;
+        return result;
     }
 };
