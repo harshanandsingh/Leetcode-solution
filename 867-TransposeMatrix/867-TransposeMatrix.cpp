76bcea74bea72ea7a2b28a4f3f84303bcd5319a7
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2025-09-07 19:17:07 +0530

    Create 867-TransposeMatrix

---
+++ 867-TransposeMatrix/867-TransposeMatrix.cpp
@@ -0,0 +1,16 @@
+// Last updated: 9/7/2025, 7:17:07 PM
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int m = mat[0].size();
+
+        vector<vector<int>>ans(m,vector<int>(n,0));
+        for(int i=0;i<n;i++){
+            for(int j = 0;j<m;j++){
+                ans[j][i] = mat[i][j];
+            }
+        }
+        return ans;
+    }
+};
