a61fed6ace9f1f2269f701a5f868270e668674ff
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2025-09-07 13:00:12 +0530

    File updated at 9/7/2025, 1:00:13 PM

--- 23-MergekSortedLists/23-MergekSortedLists.cpp
+++ 23-MergekSortedLists/23-MergekSortedLists.cpp
@@ -1,67 +1,34 @@
-// Last updated: 9/7/2025, 12:40:55 PM
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+// Last updated: 9/7/2025, 1:00:13 PM
 class Solution {
 public:
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        int n = lists.size();
-        if(n==0) return nullptr;
-        ListNode* head = lists[0];
-        int i=1;
-        while(i<n){
-            ListNode* temp1=lists[i];
-            ListNode* temp = head;
-
-            ListNode* temp_head = nullptr;
-            ListNode* temp_tail = nullptr;
-            while(temp && temp1){
-                if(temp->val < temp1->val){
-                    if(!temp_head){
-                        temp_head = temp;
-                        // temp_tail = temp;
-                        // temp = temp->next;
-                        // temp_teil->next = nullptr;
-                    }else{
-                        temp_tail -> next = temp;
-                    }
-                    temp_tail = temp;
-                    temp = temp->next;
-                    temp_tail->next = nullptr;
-                }
-                else{
-                    if(!temp_head){
-                        temp_head = temp1;
-                        // temp_tail = temp;
-                        // temp = temp->next;
-                        // temp_teil->next = nullptr;
-                    }else{
-                        temp_tail -> next = temp1;
-                    }
-                    temp_tail = temp1;
-                    temp1 = temp1->next;
-                    temp_tail->next = nullptr;
-                }
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode dummy(0);
+        ListNode* tail = &dummy;
+        
+        while (l1 && l2) {
+            if (l1->val < l2->val) {
+                tail->next = l1;
+                l1 = l1->next;
+            } else {
+                tail->next = l2;
+                l2 = l2->next;
             }
-            if(temp){
-                if(temp_tail) temp_tail ->next = temp;
-                else temp_head = temp;
-            }
-            if(temp1){
-                if(temp_tail) temp_tail ->next = temp1;
-                else temp_head = temp1;
-            }
-
-            head = temp_head;
-            i++;
+            tail = tail->next;
         }
-        return head;
+        tail->next = l1 ? l1 : l2;
+        return dummy.next;
+    }
+
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if (lists.empty()) return nullptr;
+        int interval = 1;
+        int n = lists.size();
+        while (interval < n) {
+            for (int i = 0; i + interval < n; i += interval * 2) {
+                lists[i] = mergeTwoLists(lists[i], lists[i + interval]);
+            }
+            interval *= 2;
+        }
+        return lists[0];
     }
 };
