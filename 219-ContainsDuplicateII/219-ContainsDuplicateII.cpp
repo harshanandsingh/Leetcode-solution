e692bf1019525c1513be078c7c64b5c83313978b
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2025-09-09 09:00:36 +0530

    File updated at 9/9/2025, 9:00:35 AM

--- 219-ContainsDuplicateII/219-ContainsDuplicateII.cpp
+++ 219-ContainsDuplicateII/219-ContainsDuplicateII.cpp
@@ -1,6 +1,29 @@
+// Last updated: 9/9/2025, 9:00:35 AM
 class Solution {
 public:
+
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        // unordered map -> to store the element and its indices 
+
+        unordered_map<int,int>m;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if(m.count(nums[i])){
+                if(abs(i-m[nums[i]]) <= k) return true;
+            }
+            m[nums[i]] = i;
+        }
+        return false;
+    }
+
+
+
+
+
+
+
+
+    bool containsNearbyDuplicate1(vector<int>& nums, int k) {
         unordered_map<int,int>count;
         int n = nums.size();
         int i=0,j=0;
