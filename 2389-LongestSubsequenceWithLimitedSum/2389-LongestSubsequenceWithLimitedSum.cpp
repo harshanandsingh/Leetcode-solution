817a4dae8c973696682c8ea1fbc2e80000335880
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2025-09-10 13:18:23 +0530

    File updated at 9/10/2025, 1:18:23 PM

--- 2389-LongestSubsequenceWithLimitedSum/2389-LongestSubsequenceWithLimitedSum.cpp
+++ 2389-LongestSubsequenceWithLimitedSum/2389-LongestSubsequenceWithLimitedSum.cpp
@@ -1,26 +1,39 @@
-// Last updated: 9/10/2025, 1:02:02 PM
+// Last updated: 9/10/2025, 1:18:23 PM
 class Solution {
 public:
-    vector<int> answerQueries(vector<int>& nums, vector<int>& que) {
-        int n = nums.size();
-        int m = que.size();
-        vector<int>ans(m);
+    vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
+        sort(nums.begin(), nums.end());
 
-        sort(nums.begin() , nums.end());
+        for (int i = 1; i < nums.size(); ++i)
+            nums[i] += nums[i - 1];
 
-        for(int i=0;i<m;i++){
-            int sum = 0;
-            int j = 0;
-            while(j<n){
-                sum+=nums[j];
-                if(sum > que[i]){
-                    ans[i]=j;
-                    break;
-                }
-                j++;
-            }
-            if(j==n) ans[i]=n;
+        vector<int> ans;
+        for (auto query : queries)
+        {
+            int idx = binarySearch(nums, query);
+            ans.push_back(idx);
         }
+
         return ans;
+    }
+
+    int binarySearch(vector<int>& arr, int target)
+    {
+        int left = 0, right = arr.size();
+
+        while (left < right)
+        {
+            int mid = left + (right - left) / 2;
+            
+            if ( arr[mid] == target )
+                return mid + 1;
+            
+            if ( arr[mid] > target )
+                right = mid;
+            else
+                left = mid + 1;
+        }
+
+        return left;
     }
 };
