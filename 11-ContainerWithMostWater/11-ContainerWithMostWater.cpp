1137af35c6ec4323d688ddb91300f18901fdbed8
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2025-09-24 23:29:01 +0530

    File updated at 9/24/2025, 11:29:00 PM

--- 11-ContainerWithMostWater/11-ContainerWithMostWater.cpp
+++ 11-ContainerWithMostWater/11-ContainerWithMostWater.cpp
@@ -1,17 +1,21 @@
-// Last updated: 8/8/2025, 4:12:31 PM
+// Last updated: 9/24/2025, 11:29:00 PM
 class Solution {
 public:
     int maxArea(vector<int>& a) {
         int i=0,j=a.size()-1;
         int cap = 0 , max_cap = 0;
         while(i<j){
-            cap = (j-i)*min(a[i],a[j]);
-            max_cap = max(cap,max_cap);
-            if(a[i]<a[j]) i++;
-            else if(a[i]>a[j]) j--;
+            int dis = j-i;
+            if(a[i]<a[j]){
+                cap = dis * a[i];
+                i++;
+            }
             else{
+                cap = dis * a[j];
                 j--;
             }
+         
+            max_cap = max(cap,max_cap);
         }
         return max_cap;
     }
