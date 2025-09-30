343f7491e6673fb8611e4e26c4ffdf0e0bdc3837
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2025-09-30 11:14:26 +0530

    Create 678-ValidParenthesisString

---
+++ 678-ValidParenthesisString/678-ValidParenthesisString.cpp
@@ -0,0 +1,25 @@
+// Last updated: 9/30/2025, 11:14:25 AM
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int n = s.size();
+        int low = 0, high = 0;
+
+        for(int i = 0; i < n; i++){
+            if(s[i] == '('){
+                low++;
+                high++;
+            }
+            else if(s[i] == ')'){
+                if(low > 0) low--;  // use one '(' if possible
+                high--;
+            }
+            else { // '*'
+                if(low > 0) low--;  // treat as ')'
+                high++;             // or treat as '('
+            }
+            if(high < 0) return false;  // too many ')'
+        }
+        return low == 0;
+    }
+};
