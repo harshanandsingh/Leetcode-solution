d8dca1f96fff4c0410ec90d8fb65e978ef95bba2
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2025-10-22 12:06:42 +0530

    File updated at 10/22/2025, 12:06:42 PM

--- 1926-NearestExitfromEntranceinMaze/1926-NearestExitfromEntranceinMaze.cpp
+++ 1926-NearestExitfromEntranceinMaze/1926-NearestExitfromEntranceinMaze.cpp
@@ -1,4 +1,4 @@
-// Last updated: 10/22/2025, 12:02:08 PM
+// Last updated: 10/22/2025, 12:06:42 PM
 class Solution {
 public:
     int nearestExit(vector<vector<char>>& maze, vector<int>& start) {
@@ -10,8 +10,8 @@ public:
         queue<pair<pair<int,int>,int>>q;
         q.push({{start[0],start[1]},0}); // as we alrady know the starting position 
 
-        //vis[start[0]][start[1]]=1;
-        maze[start[0]][start[1]]='+';
+        vis[start[0]][start[1]]=1;
+        //maze[start[0]][start[1]]='+';
 
         vector<pair<int,int>>v={{-1,0},{0,-1},{1,0},{0,1}};
 
@@ -24,11 +24,12 @@ public:
             for(auto [a,b]:v){
                 int new_row = row + a;
                 int new_col = col + b;
-                if(new_row >= 0 && new_row < n && new_col >= 0 && new_col < m && maze[new_row][new_col]=='.' ){
+                if(new_row >= 0 && new_row < n && new_col >= 0 && new_col < m && maze[new_row][new_col]=='.' && !vis[new_row][new_col]){
                     //&& !vis[new_row][new_col] remove this we dont need this 
-                    //vis[new_row][new_col]=1;
+                    
                     if(new_row == 0 || new_row == n-1 || new_col == 0 || new_col == m-1) return cost+1;
-                    maze[new_row][new_col]='+';
+                    //maze[new_row][new_col]='+';
+                    vis[new_row][new_col]=1;
                     q.push({{new_row,new_col},cost+1});
                 }
             }
