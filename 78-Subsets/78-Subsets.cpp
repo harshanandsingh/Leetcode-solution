39241dff74df6b9a4e3e0fc96eb347a75c2ae4dd
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2025-11-11 18:48:06 +0530

    File updated at 11/11/2025, 6:48:06 PM

--- 78-Subsets/78-Subsets.cpp
+++ 78-Subsets/78-Subsets.cpp
@@ -1,23 +1,39 @@
-// Last updated: 11/11/2025, 6:20:57 PM
+// Last updated: 11/11/2025, 6:48:06 PM
 class Solution {
 public:
-    vector<vector<int>>ans;
-    void solve(vector<int>&v,int start,vector<int>&nums){
-        if(start==nums.size()){
-            ans.push_back(v);
+    struct State
+    {
+        int pos;
+        std::vector<int> subSet;
+    };
+
+    void BackTracing(std::vector<int> &nums, std::vector<std::vector<int>> &result, State &s)
+    {
+        if (s.pos == nums.size())
+        {
+            result.push_back(s.subSet);
             return;
         }
-        
-        v.push_back(nums[start]);
-        solve(v,start+1,nums);
-        v.pop_back();
-        solve(v,start+1,nums);
 
+        // select pos
+        s.subSet.push_back(nums[s.pos]);
+        s.pos++;
+        BackTracing(nums, result, s);
+        s.pos--;
+        s.subSet.pop_back();
+
+        //donot select pos
+        s.pos++;
+        BackTracing(nums, result, s);
+        s.pos--;
     }
-    vector<vector<int>> subsets(vector<int>& nums) {
-        // in this you have two option -> include or not include the ith index
-        vector<int>v;
-        solve(v,0,nums);
-        return ans;
+
+    std::vector<std::vector<int>> subsets(std::vector<int> &nums)
+    {
+        State s;
+        s.pos = 0;
+        std::vector<std::vector<int>> result;
+        BackTracing(nums, result, s);
+        return result;
     }
 };
