f174eae8cf70166f03c3ad0276a5b7cd5129d265
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2025-11-12 18:45:34 +0530

    File updated at 11/12/2025, 6:45:33 PM

--- 338-CountingBits/338-CountingBits.cpp
+++ 338-CountingBits/338-CountingBits.cpp
@@ -1,16 +1,15 @@
-// Last updated: 11/12/2025, 6:30:01 PM
+// Last updated: 11/12/2025, 6:45:33 PM
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int>v(n+1,0);
-        for(int i=1;i<=n;i++){
-            int num = i;
-            // method to coutn number of bit 
-            while(num){
-                v[i]++;
-                num&=num-1;
-            }
+        vector<int> bits(n + 1); // size n + 1 vector
+        int set_bits = 0;
+        bits[0] = 0;
+
+        for (int i = 1; i <= n; ++i) {
+            bits[i] = bits[i >> 1] + (i & 1);
         }
-        return v;
+        return bits;
+
     }
 };
