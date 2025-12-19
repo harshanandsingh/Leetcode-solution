3948472561f04808ed235e1f55c4d8f7dafbcd5d
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2025-12-19 11:31:21 +0530

    File updated at 12/19/2025, 11:31:22 AM

--- 83-RemoveDuplicatesfromSortedList/83-RemoveDuplicatesfromSortedList.cpp
+++ 83-RemoveDuplicatesfromSortedList/83-RemoveDuplicatesfromSortedList.cpp
@@ -1,40 +1,40 @@
-// Last updated: 8/18/2025, 6:21:53 PM
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-class Solution {
-public:
-    ListNode* deleteDuplicates(ListNode* head) {
-        if(!head) return head;
-        ListNode* i = head;
-        ListNode* j= head->next;
-
-        while(j){
-            int a = i->val;
-            while(j && j->val == a){
-                j=j->next;
-            }
-            i->next=j;
-            i=j;
-            if(j) j=j->next;
-        }
-        return head;
-
-        // while(j != nullptr){
-        //     if(j->val != i-> val){
-        //         i-> next = j;
-        //         i=j;
-        //     }
-        //     else j = j->next;
-        // }
-        // i->next = j;
-        // return head;
-    }
-};
+// Last updated: 12/19/2025, 11:31:22 AM
+1/**
+2 * Definition for singly-linked list.
+3 * struct ListNode {
+4 *     int val;
+5 *     ListNode *next;
+6 *     ListNode() : val(0), next(nullptr) {}
+7 *     ListNode(int x) : val(x), next(nullptr) {}
+8 *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+9 * };
+10 */
+11class Solution {
+12public:
+13    ListNode* deleteDuplicates(ListNode* head) {
+14        if(!head) return head;
+15        ListNode* i = head;
+16        ListNode* j= head;
+17
+18        while(j){
+19            int a = i->val;
+20            while(j && j->val == a){
+21                j=j->next;
+22            }
+23            i->next=j;
+24            i=j;
+25            if(j) j=j->next;
+26        }
+27        return head;
+28
+29        // while(j != nullptr){
+30        //     if(j->val != i-> val){
+31        //         i-> next = j;
+32        //         i=j;
+33        //     }
+34        //     else j = j->next;
+35        // }
+36        // i->next = j;
+37        // return head;
+38    }
+39};
