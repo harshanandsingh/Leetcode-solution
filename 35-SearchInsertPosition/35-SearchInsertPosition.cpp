1f3c096b67b618c6a5bc82aa5f82384df7b6240b
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2026-01-25 18:34:24 +0530

    File updated at 1/25/2026, 6:34:24 PM

--- 35-SearchInsertPosition/35-SearchInsertPosition.cpp
+++ 35-SearchInsertPosition/35-SearchInsertPosition.cpp
@@ -1,19 +1,19 @@
-// Last updated: 9/13/2025, 1:05:38 PM
-class Solution {
-public:
-    int searchInsert(vector<int>& nums, int target) {
-        int n = nums.size();
-        
-        if(nums[n-1] < target) return n;
-
-        int l=0,r=n-1;
-        
-        while(l<r){
-            int mid=(l+r)/2;
-            if(nums[mid]==target) return mid;
-            else if(nums[mid]<target)  l=mid+1;
-            else r=mid;
-        }
-        return r;
-    }
-};
+// Last updated: 1/25/2026, 6:34:24 PM
+1class Solution {
+2public:
+3    int searchInsert(vector<int>& nums, int target) {
+4        int n = nums.size();
+5        
+6        if(nums[n-1] < target) return n;
+7
+8        int l=0,r=n-1;
+9        
+10        while(l<=r){
+11            int mid=(l+r)/2;
+12            if(nums[mid]==target) return mid;
+13            else if(nums[mid]<target)  l=mid+1;
+14            else r=mid-1;
+15        }
+16        return l;
+17    }
+18};
