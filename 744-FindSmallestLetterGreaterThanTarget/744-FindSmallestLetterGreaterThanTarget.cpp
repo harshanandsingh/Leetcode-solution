fb75940746abf537be50011509c9c3edf5134279
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2026-01-25 19:03:25 +0530

    File updated at 1/25/2026, 7:03:25 PM

--- 744-FindSmallestLetterGreaterThanTarget/744-FindSmallestLetterGreaterThanTarget.cpp
+++ 744-FindSmallestLetterGreaterThanTarget/744-FindSmallestLetterGreaterThanTarget.cpp
@@ -1,17 +1,18 @@
-// Last updated: 5/1/2025, 7:18:38 PM
-class Solution {
-public:
-    char nextGreatestLetter(vector<char>& letters, char target) {
-        int low = 0, high = letters.size() - 1;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (letters[mid] > target)
-                high = mid - 1;
-            else
-                low = mid + 1;
-        }
-        // low is now the first index where letters[low] > target
-        // or past the end (wrap around)
-        return low < letters.size() ? letters[low] : letters[0];
-    }
-};
+// Last updated: 1/25/2026, 7:03:25 PM
+1class Solution {
+2public:
+3    char nextGreatestLetter(vector<char>& letters, char target) {
+4        int low = 0, high = letters.size() - 1;
+5        while (low < high) {
+6            int mid = low + (high - low) / 2;
+7            if (letters[mid] > target)
+8                high = mid;
+9            else
+10                low = mid + 1;
+11        }      
+12        return letters[high]>target ? letters[high] : letters[0];
+13
+14
+15        //return high < letters.size() ? letters[high] : letters[0];
+16    }
+17};
