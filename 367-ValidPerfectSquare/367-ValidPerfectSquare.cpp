ef35f65e9762336c1e8d893baa978f230e559f65
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2026-01-25 21:29:50 +0530

    File updated at 1/25/2026, 9:29:49 PM

--- 367-ValidPerfectSquare/367-ValidPerfectSquare.cpp
+++ 367-ValidPerfectSquare/367-ValidPerfectSquare.cpp
@@ -1,14 +1,17 @@
-// Last updated: 1/25/2026, 7:09:40 PM
+// Last updated: 1/25/2026, 9:29:49 PM
 1class Solution {
 2public:
 3    bool isPerfectSquare(int num) {
-4        int low = 1,high=num;
-5        while(low<=high){
-6            long long mid = low + (high - low)/2;
-7            if(mid * mid == num) return true;
-8            else if(mid*mid > num) high = mid -1;
-9            else low = mid+1;
-10        }
-11        return false;
-12    }
-13};
+4        // int low = 1,high=num;
+5        // while(low<=high){
+6        //     long long mid = low + (high - low)/2;
+7        //     if(mid * mid == num) return true;
+8        //     else if(mid*mid > num) high = mid -1;
+9        //     else low = mid+1;
+10        // }
+11        // return false;
+12        int a = sqrt(num);
+13        if(a*a==num) return true;
+14        return false;
+15    }
+16};
