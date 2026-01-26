1f71499d0b3c6292ddab6e0182d2a20c174747d2
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2026-01-26 22:54:18 +0530

    File updated at 1/26/2026, 10:54:18 PM

--- 796-RotateString/796-RotateString.cpp
+++ 796-RotateString/796-RotateString.cpp
@@ -1,21 +1,23 @@
-class Solution {
-public:
-    bool rotateString(string s, string goal) {
-        // int n = s.length();
-        // for(int i=0;i<n;i++){
-        //     char temp=s[0];
-        //     for(int j=0;j<n-1;j++){
-        //         s[j]=s[j+1];
-        //     }
-        //     s[n-1]=temp;
-
-        //     if(s == goal) return true;
-        // }
-        // return false;
-
-        if(s.size() != goal.size()) return false;
-        s=s+s;
-        if(s.find(goal) != std::string::npos) return true;
-        return false;
-    }
-};
+// Last updated: 1/26/2026, 10:54:18 PM
+1class Solution {
+2public:
+3    bool rotateString(string s, string goal) {
+4       
+5        // if(s.size() != goal.size()) return false;
+6        // s=s+s;
+7        // if(s.find(goal) != std::string::npos) return true;
+8        // return false;
+9
+10        // ---------- or --------
+11        if(s.size() != goal.size()) return false;
+12        if(s==goal) return true;
+13        int n = s.size();
+14        for(int i=1;i<n;i++){
+15            string t = s.substr(i)+s.substr(0,i);
+16            //cout<<t<<endl;
+17            if(goal==t) return true;
+18        }
+19        return false;
+20    }
+21
+22};
