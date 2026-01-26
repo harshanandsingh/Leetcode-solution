71b97f6a95a267b7001c584a15bbf870d1de2cc0
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2026-01-26 10:10:09 +0530

    File updated at 1/26/2026, 10:10:12 AM

--- 2226-MaximumCandiesAllocatedtoKChildren/2226-MaximumCandiesAllocatedtoKChildren.cpp
+++ 2226-MaximumCandiesAllocatedtoKChildren/2226-MaximumCandiesAllocatedtoKChildren.cpp
@@ -1,32 +1,33 @@
-class Solution {
-public:
-    bool isPossible(vector<int>& candies, long long k, int x) {
-        long long count = 0;
-        for (int c : candies) {
-            count += c / x;  // Number of piles of size `x` we can create
-            if (count >= k) return true;  // If we have enough, no need to check further
-        }
-        return count >= k;
-    }
-
-    int maximumCandies(vector<int>& candies, long long k) {
-        long long sum = 0;
-        for (int c : candies) sum += c;
-        
-        if (sum < k) return 0; // If total candies are less than k, return 0
-
-        int low = 1, high = *max_element(candies.begin(), candies.end()), ans = 0;
-        
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (isPossible(candies, k, mid)) {
-                ans = mid; // Store the best possible value
-                low = mid + 1; // Try for a larger value
-            } else {
-                high = mid - 1; // Try for a smaller value
-            }
-        }
-        
-        return ans;
-    }
-};
+// Last updated: 1/26/2026, 10:10:12 AM
+1class Solution {
+2public:
+3    bool isPossible(vector<int>& candies, long long k, int x) {
+4        long long count = 0;
+5        for (int c : candies) {
+6            count += c / x;  // Number of piles of size `x` we can create
+7            if (count >= k) return true;  // If we have enough, no need to check further
+8        }
+9        return count >= k;
+10    }
+11
+12    int maximumCandies(vector<int>& candies, long long k) {
+13        // long long sum = 0;
+14        // for (int c : candies) sum += c;
+15        
+16        // if (sum < k) return 0; // If total candies are less than k, return 0
+17
+18        int low = 1, high = *max_element(candies.begin(), candies.end()), ans = 0;
+19        
+20        while (low <= high) {
+21            int mid = low + (high - low) / 2;
+22            if (isPossible(candies, k, mid)) {
+23                ans = mid; // Store the best possible value
+24                low = mid + 1; // Try for a larger value
+25            } else {
+26                high = mid - 1; // Try for a smaller value
+27            }
+28        }
+29        
+30        return ans;
+31    }
+32};
