933e459ee330b0d1a40ee32f237487c76f61f4fc
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2026-01-26 12:51:09 +0530

    File updated at 1/26/2026, 12:51:11 PM

--- 1662-CheckIfTwoStringArraysareEquivalent/1662-CheckIfTwoStringArraysareEquivalent.cpp
+++ 1662-CheckIfTwoStringArraysareEquivalent/1662-CheckIfTwoStringArraysareEquivalent.cpp
@@ -1,15 +1,16 @@
-// Last updated: 1/26/2026, 12:33:41 PM
-1class Solution {
-2public:
-3    string merg_string(vector<string> word){
-4        string s1="";
-5        for(string x:word){
-6            s1+=x;
-7        }
-8        return s1;
-9    }
-10    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
-11        
-12        return merg_string(word1)==merg_string(word2);
-13    }
-14};
+// Last updated: 1/26/2026, 12:51:11 PM
+class Solution {
+public:
+    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+        string sum1="";
+        for(int i=0;i<word1.size();i++){
+            sum1 += word1[i];
+        }
+        string sum2="";
+        for(int i=0;i<word2.size();i++){
+            sum2 += word2[i];
+        }
+        if(sum1==sum2) return true;
+        return false;
+    }
+};
