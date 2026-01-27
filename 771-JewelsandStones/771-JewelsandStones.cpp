a6b0b800da87b810a0163cd85be1daeff5a005b4
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2026-01-27 22:18:45 +0530

    File updated at 1/27/2026, 10:18:44 PM

--- 771-JewelsandStones/771-JewelsandStones.cpp
+++ 771-JewelsandStones/771-JewelsandStones.cpp
@@ -1,18 +1,18 @@
-// Last updated: 9/8/2025, 2:47:23 PM
-class Solution {
-public:
-    int numJewelsInStones(string jewels, string stones) {
-        unordered_set<int>s;
-
-        for(auto x:jewels){
-            s.insert(x);
-        }
-
-        int ans = 0;
-        for(auto x:stones){
-            if(s.count(x)) ans++;
-        }
-
-        return ans;
-    }
-};
+// Last updated: 1/27/2026, 10:18:44 PM
+1class Solution {
+2public:
+3    int numJewelsInStones(string jewels, string stones) {
+4        unordered_set<int>s(jewels.begin(),jewels.end());
+5
+6        // for(auto x:jewels){
+7        //     s.insert(x);
+8        // }
+9
+10        int ans = 0;
+11        for(auto x:stones){
+12            if(s.count(x)) ans++;
+13        }
+14
+15        return ans;
+16    }
+17};
