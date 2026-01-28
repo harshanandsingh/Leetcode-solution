e346ff61d7ab20807c39437ae9800c62422b29af
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2026-01-28 06:00:50 +0530

    File updated at 1/28/2026, 6:00:51 AM

--- 567-PermutationinString/567-PermutationinString.cpp
+++ 567-PermutationinString/567-PermutationinString.cpp
@@ -1,33 +1,55 @@
-// Last updated: 7/27/2025, 12:09:25 PM
-class Solution {
-public:
-    bool checkInclusion(string s1, string s2) {
-
-        if(s1.size()>s2.size()) return false;
-        int n = s2.size();
-
-        int k = s1.size(); // this is the size of the window 
-
-        vector<int>s1_f(26,0); // it will help to count the frequency of s1 string 
-        vector<int>s2_f(26,0); // it will help to find that the s2 contains s1 or not andy permutaion of that 
-
-        for(auto x:s1){ 
-            s1_f[x-'a']++;
-        }
-        int j = 0;
-        while(j<n){
-
-            s2_f[s2[j]-'a']++;
-
-            if(j >= k){
-                s2_f[s2[j-k]-'a']--;
-            }
-
-            if(s1_f == s2_f) return true;
-
-            j++;
-
-        }
-        return false;
-    }
-};
+// Last updated: 1/28/2026, 6:00:51 AM
+1class Solution {
+2public:
+3    bool checkInclusion(string p, string s) {
+4        if(p.size()>s.size()) return {};
+5            vector<int>pat(26);
+6            for(auto x:p) pat[x-'a']++;
+7
+8            int k = p.size(); // size of the window 
+9            int n = s.size(); // size of the string 
+10
+11            vector<int>str(26); 
+12            for(int i=0;i<k;i++){
+13                str[s[i]-'a']++; 
+14            }
+15            //vector<int>ans;
+16            if(pat==str) return true;
+17
+18            for(int i=k;i<n;i++){
+19                str[s[i]-'a']++;
+20                str[s[i-k]-'a']--;
+21                if(pat==str) return true;
+22            }
+23            return false;
+24    }
+25    bool checkInclusion1(string s1, string s2) {
+26
+27        if(s1.size()>s2.size()) return false;
+28        int n = s2.size();
+29
+30        int k = s1.size(); // this is the size of the window 
+31
+32        vector<int>s1_f(26,0); // it will help to count the frequency of s1 string 
+33        vector<int>s2_f(26,0); // it will help to find that the s2 contains s1 or not andy permutaion of that 
+34
+35        for(auto x:s1){ 
+36            s1_f[x-'a']++;
+37        }
+38        int j = 0;
+39        while(j<n){
+40
+41            s2_f[s2[j]-'a']++;
+42
+43            if(j >= k){
+44                s2_f[s2[j-k]-'a']--;
+45            }
+46
+47            if(s1_f == s2_f) return true;
+48
+49            j++;
+50
+51        }
+52        return false;
+53    }
+54};
