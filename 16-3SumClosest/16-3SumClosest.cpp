2fceba961d2705b1563868a8c26affc85406687d
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2026-01-29 16:23:48 +0530

    File updated at 1/29/2026, 4:23:49 PM

--- 16-3SumClosest/16-3SumClosest.cpp
+++ 16-3SumClosest/16-3SumClosest.cpp
@@ -1,28 +1,28 @@
-// Last updated: 8/31/2025, 11:48:50 AM
-class Solution {
-public:
-    int threeSumClosest(vector<int>& nums, int target) {
-        int n = nums.size();
-
-        sort(nums.begin(),nums.end());
-
-        int min_c = 100000;
-
-        for(int i=0;i<n;i++){
-
-            int j = i+1 , k = n-1;
-            while(j<k){
-                int sum = nums[i]+nums[j]+nums[k];
-
-                if(abs(target-sum) < abs(target-min_c) ) min_c = sum;
-
-                if(sum > target) k--;
-                else if(sum<target) j++;
-                else return target;
-
-                
-            }
-        }
-        return min_c;
-    }
-};
+// Last updated: 1/29/2026, 4:23:49 PM
+1class Solution {
+2public:
+3    int threeSumClosest(vector<int>& nums, int target) {
+4        int n = nums.size();
+5
+6        sort(nums.begin(),nums.end());
+7
+8        int min_c = 100000;
+9
+10        for(int i=0;i<n;i++){
+11
+12            int j = i+1 , k = n-1;
+13            while(j<k){
+14                int sum = nums[i]+nums[j]+nums[k];
+15
+16                if(abs(target-sum) < abs(target-min_c) ) min_c = sum;
+17
+18                if(sum > target) k--;
+19                else if(sum<target) j++;
+20                else return target;
+21
+22                
+23            }
+24        }
+25        return min_c;
+26    }
+27};
