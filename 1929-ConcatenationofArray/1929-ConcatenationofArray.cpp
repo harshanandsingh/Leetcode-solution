a47cd546356e80ea72f4addd86d6181a127a7d0a
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2026-02-26 11:32:15 +0530

    File updated at 2/26/2026, 11:32:15 AM

--- 1929-ConcatenationofArray/1929-ConcatenationofArray.cpp
+++ 1929-ConcatenationofArray/1929-ConcatenationofArray.cpp
@@ -1,12 +1,20 @@
-// Last updated: 7/26/2025, 6:13:44 AM
-class Solution {
-public:
-    vector<int> getConcatenation(vector<int>& nums) {
-        int n= nums.size();
-        // vector<int>v(2*n);
-        for(int i=0;i<n;i++){
-            nums.push_back(nums[i]);
-        }
-        return nums;
-    }
-};
+// Last updated: 2/26/2026, 11:32:15 AM
+1class Solution {
+2public:
+3    vector<int> getConcatenation(vector<int>& nums) {
+4        
+5        int n = nums.size();
+6        vector<int>ans(2*n);
+7        for(int i=0;i<n;i++){
+8            ans[i]=nums[i];
+9            ans[i+n]=nums[i];
+10        }
+11        return ans;
+12        // int n= nums.size();
+13        // // vector<int>v(2*n);
+14        // for(int i=0;i<n;i++){
+15        //     nums.push_back(nums[i]);
+16        // }
+17        // return nums;
+18    }
+19};
