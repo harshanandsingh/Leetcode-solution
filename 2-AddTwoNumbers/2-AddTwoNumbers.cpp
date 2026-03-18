054e2fb262f628e27aa4e4e84fd3c7b03b9006d4
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2026-03-18 23:41:16 +0530

    File updated at 3/18/2026, 11:41:15 PM

--- 2-AddTwoNumbers/2-AddTwoNumbers.cpp
+++ 2-AddTwoNumbers/2-AddTwoNumbers.cpp
@@ -1,45 +1,55 @@
-// Last updated: 9/7/2025, 5:44:29 PM
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-class Solution {
-public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        if(!l1) return l2;
-        if(!l2) return l1;
-        ListNode *res = new ListNode(0);
-        ListNode *temp = res;
-        int carry = 0;
-        while(l1 || l2 || carry){
-
-            int sum = carry;
-            if(l1){
-                sum+=l1->val;
-                l1 = l1->next;
-            }
-            if(l2){
-                sum+=l2->val;
-                l2 = l2->next;
-            }
-
-            carry = sum / 10;
-            temp->next = new ListNode(sum%10);
-            temp = temp->next;
-            // int sum = l1->val + l2->val + carry;
-            // int val = sum %10;
-            // carry = sum/10;
-            // temp->next = new ListNode(val);
-            // temp = temp->next;
-            // l1 = l1->next;
-            // l2 = l2->next;
-        }
-        return res->next;
-    }
-};
+// Last updated: 3/18/2026, 11:41:15 PM
+1/**
+2 * Definition for singly-linked list.
+3 * struct ListNode {
+4 *     int val;
+5 *     ListNode *next;
+6 *     ListNode() : val(0), next(nullptr) {}
+7 *     ListNode(int x) : val(x), next(nullptr) {}
+8 *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+9 * };
+10 */
+11class Solution {
+12public:
+13    ListNode* rev(ListNode* head){
+14        if(!head) return head;
+15
+16        ListNode *curr = head,*pre = nullptr;
+17        while(curr){
+18            ListNode* next = curr->next;
+19            curr->next = pre;
+20            pre = curr;
+21            curr = next;
+22        }
+23        return pre;
+24    }
+25    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+26        if(!l1) return l2;
+27        if(!l2) return l1;
+28        // cout<<l1->val<<endl;
+29        //l1 = rev(l1);
+30        //l2 = rev(l2);
+31        // cout<<l1->val<<endl;
+32        ListNode *res = new ListNode(0);
+33        ListNode *temp = res;
+34        int carry = 0;
+35        while(l1 || l2 || carry){
+36
+37            int sum = carry;
+38            
+39            if(l1){
+40                sum+=l1->val;
+41                l1 = l1->next;
+42            }
+43            if(l2){
+44                sum+=l2->val;
+45                l2 = l2->next;
+46            }
+47            cout<<sum<<endl;
+48            carry = sum / 10;
+49            temp->next = new ListNode(sum%10);
+50            temp = temp->next;
+51        }
+52        return res->next;
+53    }
+54};
