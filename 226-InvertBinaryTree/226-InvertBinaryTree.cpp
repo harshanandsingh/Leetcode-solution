0db1d428d3dcf38167a5b8bb3605c08a58c1dfaa
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2026-07-31 12:02:09 +0530

    File updated at 7/31/2026, 12:02:08 PM

--- 226-InvertBinaryTree/226-InvertBinaryTree.cpp
+++ 226-InvertBinaryTree/226-InvertBinaryTree.cpp
@@ -1,26 +1,28 @@
-// Last updated: 11/10/2025, 1:12:53 PM
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
-public:
-    TreeNode* invertTree(TreeNode* root) {
-        if(!root) return nullptr;
-
-        invertTree(root->left);
-        invertTree(root->right);
-
-        TreeNode* temp = root->left;
-        root->left = root->right;
-        root->right = temp;
-        return root;
-    }
-};
+// Last updated: 7/31/2026, 12:02:08 PM
+1/**
+2 * Definition for a binary tree node.
+3 * struct TreeNode {
+4 *     int val;
+5 *     TreeNode *left;
+6 *     TreeNode *right;
+7 *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+8 *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+9 *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+10 * };
+11 */
+12class Solution {
+13public:
+14    void invert(TreeNode* root){
+15        if(root == nullptr ) return ;
+16        TreeNode* temp = root->left;
+17        root->left = root->right;
+18        root->right = temp;
+19        invert(root->left);
+20        invert(root->right);
+21    }
+22    TreeNode* invertTree(TreeNode* root) {
+23        if(root == nullptr) return nullptr;
+24        invert(root);
+25        return root;
+26    }
+27};
