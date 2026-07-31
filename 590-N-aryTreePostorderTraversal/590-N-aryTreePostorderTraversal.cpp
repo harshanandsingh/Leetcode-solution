94779de494f9516959f6bb17e5a344fab49aea8d
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2026-07-31 11:47:14 +0530

    File updated at 7/31/2026, 11:47:12 AM

--- 590-N-aryTreePostorderTraversal/590-N-aryTreePostorderTraversal.cpp
+++ 590-N-aryTreePostorderTraversal/590-N-aryTreePostorderTraversal.cpp
@@ -1,43 +1,43 @@
-// Last updated: 11/14/2025, 11:34:24 AM
-class Solution {
-public:
-    vector<int>ans;
-    void traversal(Node* root){
-        if(!root) return;
-        vector<Node*>temp = root->children;
-        for(auto x:temp){
-           traversal(x);
-        }
-        ans.push_back(root->val);
-    }
-    vector<int> postorder(Node* root) {
-        traversal(root);
-        return ans;
-        
-       // cout<<root->val<<endl;
-        // vector<Node*>temp = root->children;
-        // for(auto x:temp){
-        //    if(x != nullptr) cout<<x->val<<endl;
-        // }
-        // return {};
-    }
-};
-/*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    vector<Node*> children;
-
-    Node() {}
-
-    Node(int _val) {
-        val = _val;
-    }
-
-    Node(int _val, vector<Node*> _children) {
-        val = _val;
-        children = _children;
-    }
-};
-*/
+// Last updated: 7/31/2026, 11:47:12 AM
+1/*
+2// Definition for a Node.
+3class Node {
+4public:
+5    int val;
+6    vector<Node*> children;
+7
+8    Node() {}
+9
+10    Node(int _val) {
+11        val = _val;
+12    }
+13
+14    Node(int _val, vector<Node*> _children) {
+15        val = _val;
+16        children = _children;
+17    }
+18};
+19*/
+20
+21class Solution {
+22public:
+23    void n_postorder(Node* root,vector<int>&ans){
+24        if(root == nullptr) return;
+25
+26        for(auto x:root->children){
+27            n_postorder(x,ans);
+28            ans.push_back(x->val);
+29        }
+30       
+31
+32    }
+33    vector<int> postorder(Node* root) {
+34        if(root == nullptr) return {};
+35
+36        vector<int>ans;
+37
+38        n_postorder(root,ans);
+39        ans.push_back(root->val);
+40        return ans;
+41    }
+42};
