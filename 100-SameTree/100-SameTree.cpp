ba01eb50e40d9d46ccbf9d202744ac7078909f8a
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2026-08-04 17:28:59 +0530

    File updated at 8/4/2026, 5:29:00 PM

--- 100-SameTree/100-SameTree.cpp
+++ 100-SameTree/100-SameTree.cpp
@@ -1,74 +1,22 @@
-// Last updated: 8/21/2025, 5:33:59 PM
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
-public:
-    bool isSameTree(TreeNode* p, TreeNode* q) {
-        if(!p && !q) return true;
-        if(!p || !q || p->val != q->val) return false;
-        return isSameTree(p->left , q->left) && isSameTree(p->right , q->right);
-    }
-    // this is correct code -> in abouve code we are trying to check or learn dfs in two tree 
-    bool isSameTree1(TreeNode* p, TreeNode* q) {
-        // we have to check two tree are same or not 
-        // i think we cant do dfs on both the tree at the same time 
-        // so lets do bfs 
-
-        queue<TreeNode*> q1, q2;
-        q1.push(p);
-        q2.push(q);
-
-        while(!q1.empty() && !q2.empty()) {
-            TreeNode* a = q1.front(); q1.pop();
-            TreeNode* b = q2.front(); q2.pop();
-
-            if((a == nullptr && b != nullptr) || (a != nullptr && b == nullptr))
-                return false;
-
-            if(a && b) {
-                if(a->val != b->val) return false;
-                q1.push(a->left);
-                q1.push(a->right);
-                q2.push(b->left);
-                q2.push(b->right);
-            }
-        }
-        return q1.empty() && q2.empty();
-
-        // queue<TreeNode*>q1,q2;
-        // if(p) q1.push(p);
-        // if(q) q2.push(q);
-
-        // while(!q1.empty() && !q2.empty()){
-        //     int n = q1.size();
-        //     int m = q2.size();
-        //     if(n != m) return false;
-        //     while(n--){
-        //         TreeNode *a , *b;
-        //         a = q1.front();
-        //         b = q2.front();
-        //         q1.pop(); q2.pop();
-        //         if((a == nullptr && b != nullptr) || (a != nullptr && b == nullptr))
-        //             return false;
-        //         if(a && b && a->val != b->val)
-        //             return false;
-
-        //         if(a) q1.push(a->left);
-        //         if(a) q1.push(a->right);
-        //         if(b) q2.push(b->left);
-        //         if(b) q2.push(b->right);
-        //     }
-        // }
-        // if( (q1.empty() && !q2.empty() ) || (!q1.empty() && q2.empty()) ) return false;
-        // return true;
-    }
-};
+// Last updated: 8/4/2026, 5:29:00 PM
+1/**
+2 * Definition for a binary tree node.
+3 * struct TreeNode {
+4 *     int val;
+5 *     TreeNode *left;
+6 *     TreeNode *right;
+7 *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+8 *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+9 *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+10 * };
+11 */
+12class Solution {
+13public:
+14    bool isSameTree(TreeNode* p, TreeNode* q) {
+15        if( (p==nullptr && q != nullptr) || (p!=nullptr && q == nullptr)) return false;
+16        else if(p == nullptr && q==nullptr) return true;
+17        
+18        if(p->val != q->val) return false;
+19        return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
+20    }
+21};
