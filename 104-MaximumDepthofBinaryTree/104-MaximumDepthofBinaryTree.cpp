e12809ed3da8baf168c57233192b32df0912770b
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2026-08-04 16:00:05 +0530

    File updated at 8/4/2026, 4:00:06 PM

--- 104-MaximumDepthofBinaryTree/104-MaximumDepthofBinaryTree.cpp
+++ 104-MaximumDepthofBinaryTree/104-MaximumDepthofBinaryTree.cpp
@@ -1,23 +1,24 @@
-// Last updated: 8/14/2025, 12:30:04 PM
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
-public:
-    int maxDepth(TreeNode* root) {
-        if(!root) return 0;
-
-        int left = maxDepth(root->left);
-        int right = maxDepth(root->right);
-
-        return 1+max(left,right);
-    }
-};
+// Last updated: 8/4/2026, 4:00:06 PM
+1/**
+2 * Definition for a binary tree node.
+3 * struct TreeNode {
+4 *     int val;
+5 *     TreeNode *left;
+6 *     TreeNode *right;
+7 *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+8 *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+9 *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+10 * };
+11 */
+12class Solution {
+13public:
+14    int maxDepth(TreeNode* root) {
+15        if(root == nullptr) return 0; // base case 
+16
+17        int left = 0;
+18        left = maxDepth(root->left);
+19        int right = 0;
+20        right = maxDepth(root->right);
+21        return max(left,right) + 1;
+22    }
+23}; 
