753f13c39627eb9fa2a90847c171dba343321e4a
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2026-08-04 16:37:11 +0530

    File updated at 8/4/2026, 4:37:12 PM

--- 543-DiameterofBinaryTree/543-DiameterofBinaryTree.cpp
+++ 543-DiameterofBinaryTree/543-DiameterofBinaryTree.cpp
@@ -1,30 +1,34 @@
-// Last updated: 11/19/2025, 12:08:45 PM
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
-public:
-    int result = 0;
-    int dfs(TreeNode* root){
-        if(!root) return 0;
-
-        int left = dfs(root->left);
-        int right = dfs(root->right);
-
-        result = max(left+right , result);
-
-        return 1+max(left,right);
-    }
-    int diameterOfBinaryTree(TreeNode* root) {
-        dfs(root);
-        return result;
-    }
-};
+// Last updated: 8/4/2026, 4:37:12 PM
+1/**
+2 * Definition for a binary tree node.
+3 * struct TreeNode {
+4 *     int val;
+5 *     TreeNode *left;
+6 *     TreeNode *right;
+7 *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+8 *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+9 *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+10 * };
+11 */
+12class Solution {
+13public:
+14    int di(TreeNode* root,int &ans){
+15        if(root == nullptr) return 0;
+16
+17        int left =  di(root->left,ans);
+18        int right =  di(root->right,ans);
+19
+20        int dia = left + right;
+21        ans = max(ans,dia);
+22
+23        if(left ==0 && right ==0) return 1;
+24        return max(left,right)+1;
+25
+26    }
+27    int diameterOfBinaryTree(TreeNode* root) {
+28        if(root == nullptr) return 0;
+29        int ans = 0;
+30        di(root,ans);
+31        return ans;
+32    }
+33};
