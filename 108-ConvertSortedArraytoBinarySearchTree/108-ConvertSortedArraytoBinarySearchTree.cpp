14989350fd66207a98f6a3b9a535bd8d1b9d64d5
Harsh Anand <167998439+harshanandsingh@users.noreply.github.com>
2026-08-05 09:45:24 +0530

    File updated at 8/5/2026, 9:45:25 AM

--- 108-ConvertSortedArraytoBinarySearchTree/108-ConvertSortedArraytoBinarySearchTree.cpp
+++ 108-ConvertSortedArraytoBinarySearchTree/108-ConvertSortedArraytoBinarySearchTree.cpp
@@ -1,29 +1,27 @@
-// Last updated: 11/10/2025, 12:16:12 PM
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
-public:
-    TreeNode* tree_create(int start,int end,vector<int>&nums){
-        int mid = start + (end - start)/2;
-        // base case -> 
-        if(start > end) return nullptr;
-        
-        TreeNode* temp = new TreeNode(nums[mid]);
-        temp->left = tree_create(start,mid-1,nums);
-        temp->right = tree_create(mid+1,end,nums);
-        return temp;
-    }
-    TreeNode* sortedArrayToBST(vector<int>& nums) {
-        int n = nums.size();
-        return tree_create(0,n-1,nums);
-    }
-};
+// Last updated: 8/5/2026, 9:45:25 AM
+1/**
+2 * Definition for a binary tree node.
+3 * struct TreeNode {
+4 *     int val;
+5 *     TreeNode *left;
+6 *     TreeNode *right;
+7 *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+8 *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+9 *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+10 * };
+11 */
+12class Solution {
+13public:
+14    TreeNode* bst(vector<int>& nums,int low,int high){
+15        if(low > high) return nullptr;
+16
+17        int mid = (low + high+1) /2;
+18        TreeNode* temp = new TreeNode(nums[mid]);
+19        temp->left = bst(nums,low,mid-1);
+20        temp->right = bst(nums,mid+1,high);
+21        return temp;
+22    }
+23    TreeNode* sortedArrayToBST(vector<int>& nums) {
+24        return bst(nums,0,nums.size()-1);
+25    }
+26};
